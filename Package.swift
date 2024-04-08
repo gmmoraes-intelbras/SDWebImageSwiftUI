f65781993468656a4db7cdf47651b1d06ee857a2
gmmoraes-intelbras <109680874+gmmoraes-intelbras@users.noreply.github.com>
2024-04-08 11:35:57 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDWebImageSwiftUI",
     platforms: [
-       .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
+       .macOS(.v11), .iOS(.v12), .tvOS(.v14), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
